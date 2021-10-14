c4367c47a893433a80aa426ed665cc8f8b28726b
Sarah <sarah.mir3@hotmail.com>
2021-10-14 14:55:55 -0400

    configs

---
+++ Weather_Database/config1.py
@@ -0,0 +1,3 @@
+weather_api_key="c460fb85c2abaee3a826256f12c4827b"
+# Google API Key
+g_key = "AIzaSyD-BaD1Fte6VMwFOgTkEFFFNe3LaAtNsG0"
---
+++ config.py
@@ -0,0 +1,3 @@
+weather_api_key="e8ced405e92f2f6231261d40bc28e7dd"
+# Google API Key
+g_key = "AIzaSyD2IH_VhxHpqPrAeWXRowkBCmCqyEFXPAg"
